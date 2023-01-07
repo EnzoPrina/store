0456123fe054f463a4f07f74a568d800324c6fc8
Enzo Prina <enzoprina9@gmail.com>
2023-01-07 11:12:57 -0300

    maquetacion

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,27 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Home from "../containers/Home.jsx";
-import Checkout from "../containers/Checkout.jsx";
-import Success from "../containers/Success.jsx";
-import Payment from "../containers/Payment.jsx";
-import Information from "../containers/Information.jsx";
-import NotFound from "../containers/NotFound.jsx";
-
-import '../style/app.css';
+import Home from '../containers/Home';
+import Checkout from '../containers/Checkout';
+import Information from '../containers/Information';
+import Payment from '../containers/Payment';
+import Success from '../containers/Success';
+import NotFound from '../containers/NotFound';
+import Layout from '../components/Layout';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route index path="/" element={<Home />} />      
-        <Route  path='/checkout' element={<Checkout />} />
-        <Route  path='/checkout/information' element={<Information />} />
-        <Route  path='/checkout/payment' element={<Payment />} />
-        <Route  path='/checkout/success' element={<Success />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>    
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/checkout" component={Checkout} />
+          <Route exact path="/checkout/information" component={Information} />
+          <Route exact path="/checkout/payment" component={Payment} />
+          <Route exact path="/checkout/success" component={Success} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
